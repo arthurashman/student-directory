051c573417e1e46c9e45d87d8ab07db4293d885b
Arthur Ashman <arthurashman@gmail.com>
2019-02-25 18:28:49 +0000

    Framework for interactive menu

--- directory.rb
+++ directory.rb
@@ -1,33 +1,80 @@
-student_count = 11
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, hit return twice"
+  students = []
+  name = gets.chomp
+  if name == ""
+    name = "unknown"
+  end
+  puts "Please enter that student's cohort"
+  cohort = gets.chomp
+  if cohort == ""
+    cohort = "unknown"
+  end
 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+  while !name.empty? do
+    students << {name: name.to_sym, cohort: cohort.to_sym, DoB: :dob, gender: :gender}
+
+    if students.length == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+  name = gets.chomp
+  end
+  students
+end
+
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(150)
+  puts "-------------".center(150)
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  if students.length > 0
+    i = 0
+    while students.length > i
+      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(150)
+      i += 1
+    end
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  if students.length == 1
+    puts "Overall, we have #{students.count} great student".center(150)
+  else
+    puts "Overall, we have #{students.count} great students".center(150)
+  end
 end
 
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input new students"
+    puts "2. List the enrolled students"
+    puts "3. Exit"
+
+    selection = gets.chomp
+
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        if students.length > 0
+          print_header
+          print(students)
+          print_footer(students)
+        else
+          puts "Welcome to Villains Academy".center(150)
+          puts "Currently enrolling students".center(150)
+          puts "-------------".center(150)
+        end
+      when "9"
+      else
+        puts "Please enter a valid option"
+      end
+    end
+  end
+
+interactive_menu
