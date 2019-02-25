376f49e31e17fb54aa7b9da04e10354f7cc05267
Arthur Ashman <arthurashman@gmail.com>
2019-02-25 21:43:16 +0000

    Student directory now has a save function

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
+@students = []
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, hit return twice"
-  students = []
   name = gets.chomp
   if name == ""
     name = "unknown"
@@ -13,16 +13,66 @@ def input_students
   end
 
   while !name.empty? do
-    students << {name: name.to_sym, cohort: cohort.to_sym, DoB: :dob, gender: :gender}
+    @students << {name: name.to_sym, cohort: cohort.to_sym, DoB: :dob, gender: :gender}
 
-    if students.length == 1
-      puts "Now we have #{students.count} student"
+    if @students.length == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
   name = gets.chomp
   end
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input new students"
+  puts "2. List the enrolled students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  if @students.length > 0
+    print_header
+    print_student_list
+    print_footer
+  else
+    puts "Welcome to Villains Academy".center(150)
+    puts "Currently enrolling students".center(150)
+    puts "-------------".center(150)
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "Please enter a valid option"
+  end
 end
 
 def print_header
@@ -30,51 +80,22 @@ def print_header
   puts "-------------".center(150)
 end
 
-def print(students)
-  if students.length > 0
+def print_student_list
+  if @students.length > 0
     i = 0
-    while students.length > i
-      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(150)
+    while @students.length > i
+      puts "#{i + 1}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort)".center(150)
       i += 1
     end
   end
 end
 
-def print_footer(students)
-  if students.length == 1
-    puts "Overall, we have #{students.count} great student".center(150)
+def print_footer
+  if @students.length == 1
+    puts "Overall, we have #{@students.count} great student".center(150)
   else
-    puts "Overall, we have #{students.count} great students".center(150)
+    puts "Overall, we have #{@students.count} great students".center(150)
   end
 end
-
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input new students"
-    puts "2. List the enrolled students"
-    puts "3. Exit"
-
-    selection = gets.chomp
-
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        if students.length > 0
-          print_header
-          print(students)
-          print_footer(students)
-        else
-          puts "Welcome to Villains Academy".center(150)
-          puts "Currently enrolling students".center(150)
-          puts "-------------".center(150)
-        end
-      when "9"
-      else
-        puts "Please enter a valid option"
-      end
-    end
-  end
 
 interactive_menu
